aa4b406c20d04628657a2dfea187e35a1882ea11
Bnorr <btnorris@mail.csuchico.edu>
2019-09-04 16:17:01 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
+//Author: Brian Norris
+
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Brian. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
